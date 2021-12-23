2951488dd2785e4aa8a41b716f67669a73aef3ed
Raffaell Negreiros Guimaraes <tqi_rnegreiros@uolinc.com>
2021-12-23 18:26:30 -0300

    Clen Code

--- src/main/java/com/example/desafio1/Desafio1Application.java
+++ src/main/java/com/example/desafio1/Desafio1Application.java
@@ -12,30 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Desafio1Application {
 
-    @Autowired
-    @Qualifier("applicationName")
-    private String applicationName; //aqui está utilizando o Bean, que está os dados do arquivo config (get; set), e bem parecido com o interface do C#
-
-    @Value("${application.printConfigurations}")// aqui ele acessa e tambem está lendo o local application.properties
-    private String configurations; // aqui eu mando aonde o properties vai ser executado
-
-    @GetMapping("/hello")
-    public String helloWord() {
-        return "Hello Word";
-    }
-
-    @GetMapping("/hello-configuration")
-    public String printConfigurations() {
-        return configurations;
-    }
-
-    @GetMapping("/hello-application-properties")
-    public String printApplicationProperties() {
-        return applicationName;
-    }
-
-
     public static void main(String[] args) {
+
         SpringApplication.run(Desafio1Application.class, args);
     }
 
